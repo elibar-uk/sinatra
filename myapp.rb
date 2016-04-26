e978f8be16e0087aa7932fcc18dba739fc1937a3
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-26 11:20:06 +0100

    params

--- myapp.rb
+++ myapp.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
-get '/' do
-  'Hello Ruta'
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
-
-get '/secret' do
-  'This is a secret page'
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
